c72b691eedad0898769411820aae308e3343a87f
AmitDebadwar <amit.debadwar@gmail.com>
2014-12-24 00:12:04 +0530

    TS Simple Student Application

---
+++ TypeScript/BasicTSWithAngular/BasicTSWithAngular/app/app.ts
@@ -0,0 +1,33 @@
+﻿/// <reference path="../DefinitionFiles/jquery.d.ts" />
+/// <reference path="../DefinitionFiles/angular.d.ts" />
+
+var mod = angular.module('Main', []);
+
+class MainCtrl {
+    static $inject = ['$scope'];
+    constructor(public $scope) {
+        this.init();
+    }
+
+    public init() {
+        var self = this;
+        self.$scope.allStudent = [];
+
+        this.$scope.AddStudent = function () {
+
+            if (self.$scope.studentRoll && self.$scope.studentName) {
+
+                var student = {
+                    studentRoll: self.$scope.studentRoll,
+                    studentName: self.$scope.studentName
+                };
+                self.$scope.allStudent.push(student);              
+            }
+            else {
+                alert('Please enter student to add');
+            }
+        }
+    }
+}
+
+mod.controller("MainCtrl", MainCtrl);
