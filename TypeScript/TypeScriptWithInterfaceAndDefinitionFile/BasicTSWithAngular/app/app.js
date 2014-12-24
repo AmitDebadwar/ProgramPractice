a0e75c6872399175aaa5131f81334fd4fb492544
AmitDebadwar <amit.debadwar@gmail.com>
2014-12-24 21:48:20 +0530

    Adding ts basic angular to implement definition file, interface and modules in typescript

---
+++ TypeScript/TypeScriptWithInterfaceAndDefinitionFile/BasicTSWithAngular/app/app.js
@@ -0,0 +1,34 @@
+﻿var mod = angular.module('Main', []);
+
+var MainCtrl = (function () {
+    function MainCtrl($scope) {
+        this.$scope = $scope;
+        this.init();
+    }
+    MainCtrl.prototype.init = function () {
+        var self = this;
+        self.$scope.allStudent = [];
+
+        this.$scope.AddStudent = function () {
+            if (self.$scope.studentRoll && self.$scope.studentName) {
+                var std = new Student();
+                std.studentRoll = self.$scope.studentRoll;
+                std.studentName = self.$scope.studentName;
+
+                self.$scope.allStudent.push(std);
+            } else {
+                alert('Please enter student to add');
+            }
+        };
+    };
+    MainCtrl.$inject = ['$scope'];
+    return MainCtrl;
+})();
+
+var Student = (function () {
+    function Student() {
+    }
+    return Student;
+})();
+
+mod.controller("MainCtrl", MainCtrl);
---
+++ TypeScript/TypeScriptWithInterfaceAndDefinitionFile/BasicTSWithAngular/app/app.ts
@@ -0,0 +1,39 @@
+﻿/// <reference path="../DefinitionFiles/jquery.d.ts" />
+/// <reference path="../DefinitionFiles/angular.d.ts" />
+
+var mod = angular.module('Main', []);
+
+class MainCtrl {
+    static $inject = ['$scope'];
+    constructor(public $scope) {
+        this.init();
+    }
+
+    public init() {
+        var self = this;
+        self.$scope.allStudent = [];
+
+        this.$scope.AddStudent = function () {
+
+            if (self.$scope.studentRoll && self.$scope.studentName) {
+
+                var std = new Student();
+                std.studentRoll = self.$scope.studentRoll;
+                std.studentName = self.$scope.studentName;
+
+                self.$scope.allStudent.push(std);              
+            }
+            else {
+                alert('Please enter student to add');
+            }
+        }
+    }
+}
+
+class Student
+{
+    studentRoll: number;
+    studentName: string;
+}
+
+mod.controller("MainCtrl", MainCtrl);
