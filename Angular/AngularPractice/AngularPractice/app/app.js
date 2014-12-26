fab41f0bfd630958127585706a62d7555be80219
AmitDebadwar <amit.debadwar@gmail.com>
2014-12-26 11:57:25 +0530

    Adding a simple web application for Angular Js Activated

---
+++ Angular/AngularPractice/AngularPractice/app/app.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="../lib/angular.js" />
+
+
+var module = angular.module('MainMod', []);
+
+module.controller('MainCtrl', ['$scope', function ($scope) {
+    $scope.msg = "Activated";
+
+    var obj = {
+        num1: 3,
+        num2: 4,
+        add: function () {
+            return this.num1 + this.num2;
+        },
+
+    }
+
+
+    $scope.callMyFunction = function () {
+        alert(obj.num1);
+        obj.num1 = 7;
+        alert(obj.num1);
+
+
+        obj.num2 = 9;
+        var newAddition = obj.add();
+        alert(newAddition);
+
+    };
+
+
+
+
+
+
+
+
+
+}]);
