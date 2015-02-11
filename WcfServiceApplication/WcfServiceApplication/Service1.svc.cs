ad7544121b386be3020ad5cd409b77dd7c3ea4de
AmitDebadwar <amit.debadwar@gmail.com>
2015-02-11 21:26:27 +0530

    WCFBasicWithWINFormApplication

---
+++ WcfServiceApplication/WcfServiceApplication/Service1.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Script.Serialization;
+
+namespace WcfServiceApplication
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Service1 : IService1
+    {
+        JavaScriptSerializer js;
+
+        public Service1()
+        {
+            js = new JavaScriptSerializer();
+        }
+
+        public string GetAllProducts(string id)
+        {
+            var list = new List<Product>() { 
+             new Product(){ ProductID="111", ProductName="Nokia "+id},
+             new Product(){ ProductID="222", ProductName="Samsung "},
+             new Product(){ ProductID="333", ProductName="Mi"}
+            };
+
+            return js.Serialize(list);
+        }
+
+        public string GetProduct()
+        {
+            var obj = new Product()
+            {
+                ProductID = "1",
+                ProductName = "Nokia"
+            };
+
+            return js.Serialize(obj);
+        }
+
+        public bool SaveProductToCart(Product productToSave)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WcfServiceApplication/myFormApplication/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Web.Helpers;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
+
+namespace myFormApplication
+{
+    public partial class Form1 : Form
+    {
+        JavaScriptSerializer _serialize;
+        public Form1()
+        {
+            InitializeComponent();
+            _serialize = new JavaScriptSerializer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebRequest req2 = WebRequest.Create(@"http://pneitsh54453d:1120/Service1.svc/GetAll/2");
+            req2.Method = "POST";
+            req2.ContentLength = 0; 
+            req2.ContentType = @"application/json; charset=utf-8";
+
+            HttpWebResponse response = (HttpWebResponse)req2.GetResponse();
+           
+            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+            {
+                string jsonResponse = string.Empty;
+                jsonResponse = sr.ReadToEnd();
+                Console.WriteLine(jsonResponse);
+                var decodedJson=Json.Decode(jsonResponse);
+                var obj = _serialize.Deserialize<Product[]>(decodedJson);
+            }
+
+
+            //req2.BeginGetRequestStream(new AsyncCallback(delegate(IAsyncResult id) { }), req2);
+
+            //req2.BeginGetResponse(new AsyncCallback(delegate(IAsyncResult res) {
+
+            //}), req2);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WebRequest req2 = WebRequest.Create(@"http://pneitsh54453d:1120/Service1.svc/GetProduct");
+            req2.Method = "POST";
+            req2.ContentLength = 0;
+            req2.ContentType = @"application/json; charset=utf-8";
+
+            HttpWebResponse response = (HttpWebResponse)req2.GetResponse();
+            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+            {
+                string jsonResponse = string.Empty;
+                jsonResponse = sr.ReadToEnd();
+                Console.WriteLine(jsonResponse);
+                var decodedJson = Json.Decode(jsonResponse);
+                var obj = _serialize.Deserialize<Product>(decodedJson);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+  
+            WebRequest req2 = WebRequest.Create(@"http://glosbe.com/gapi/translate?from=eng&dest=mar&format=json&phrase=god&pretty=true");
+            req2.Method = "POST";
+            req2.ContentLength = 0;
+            req2.ContentType = @"application/json; charset=utf-8";
+
+            HttpWebResponse response = (HttpWebResponse)req2.GetResponse();
+            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+            {
+                string jsonResponse = string.Empty;
+                jsonResponse = sr.ReadToEnd();
+                var decodedJson = Json.Decode(jsonResponse);
+                //object[] obj = _serialize.Deserialize<object[]>(decodedJson);
+
+
+                string jsonString = "{\"dateStamp\":\"2010/01/01\", \"Message\": \"hello\" }";
+                dynamic myObject = JsonConvert.DeserializeObject<dynamic>(jsonString);
+                  
+                DateTime dateStamp = Convert.ToDateTime(myObject.dateStamp);
+                string Message = myObject.Message;
+            }
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CookieContainer container = new CookieContainer();
+            container.Add(new Cookie("First", "xxx") {  Domain="Abc" });
+            //container.Add(new Cookie("Second", "yyy") { });
+
+            var con = container;
+        }
+
+
+
+    }
+
+
+    public class Product
+    {
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+    }
+}
