b5aea8f3c8011ef7c831d2e4b675592adecf4631
AmitDebadwar <amit.debadwar@gmail.com>
2016-03-22 10:10:51 +0530

    recompile html in directive added comment

--- Angular/1Page/compileHTMLinStringDirective.js
+++ Angular/1Page/compileHTMLinStringDirective.js
@@ -2,6 +2,16 @@
  * Created by cwr.Amit.Debadwar on 2/1/2016.
  */
 
+/**
+ * if you have a button html in xml file or in a json.
+ * It will be rendered only but not compiled and will not execute
+ * the function of the controller.
+ *
+ * For executing the function in a controller you need to recompile directive.
+ *
+ * e.g. On common error page of SMB, relogin page.
+ */
+
 
 angular.module("smbApp").directive('commonErrorPage', function($compile, $parse) {
     return {
