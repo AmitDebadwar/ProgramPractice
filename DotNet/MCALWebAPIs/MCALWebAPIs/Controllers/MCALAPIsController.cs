11b1d8ef6e98bf56648f2496fb8e5407aac31429
AmitDebadwar <amit.debadwar@gmail.com>
2016-05-10 03:12:42 +0530

    formatted code

--- DotNet/MCALWebAPIs/MCALWebAPIs/Controllers/MCALAPIsController.cs
+++ DotNet/MCALWebAPIs/MCALWebAPIs/Controllers/MCALAPIsController.cs
@@ -23,16 +23,19 @@ namespace MCALWebAPIs.Controllers
             int skip = int.Parse(nvc["skip"]);
 
             var list= new List<StudentInfo>() { 
+          
             new StudentInfo(){ RollNo="1", Name="Rajesh"},
             new StudentInfo(){ RollNo="2", Name="Mahesh"},
             new StudentInfo(){ RollNo="3", Name="Om"},
             new StudentInfo(){ RollNo="4", Name="Disha"},
             new StudentInfo(){ RollNo="5", Name="Rahul"},
+            
             new StudentInfo(){ RollNo="6", Name="Aniket"},
             new StudentInfo(){ RollNo="7", Name="Vijaya"},
             new StudentInfo(){ RollNo="8", Name="Rajeshwar"},
             new StudentInfo(){ RollNo="9", Name="Ganesh"},
             new StudentInfo(){ RollNo="10", Name="Mudra"},
+            
             new StudentInfo(){ RollNo="11", Name="Manjusha"},
             new StudentInfo(){ RollNo="12", Name="Pankaj"},
             new StudentInfo(){ RollNo="13", Name="Pooja"},
