e4aa5fd05dea3fa3d8734dc6d672e274efc9ce45
AmitDebadwar <amit.debadwar@gmail.com>
2016-05-10 03:11:18 +0530

    server side paging done.

--- DotNet/MCALWebAPIs/MCALWebAPIs/Controllers/MCALAPIsController.cs
+++ DotNet/MCALWebAPIs/MCALWebAPIs/Controllers/MCALAPIsController.cs
@@ -1,18 +1,67 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace MCALWebAPIs.Controllers
 {
     public class MCALAPIsController : ApiController
     {
-        [HttpGet]
-        public string GetString()
+
+         
+        public JsonResult getStudents()
         {
-            return "saibaba";
+
+            NameValueCollection nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
+
+            int take = int.Parse(nvc["take"]);
+            int skip = int.Parse(nvc["skip"]);
+
+            var list= new List<StudentInfo>() { 
+            new StudentInfo(){ RollNo="1", Name="Rajesh"},
+            new StudentInfo(){ RollNo="2", Name="Mahesh"},
+            new StudentInfo(){ RollNo="3", Name="Om"},
+            new StudentInfo(){ RollNo="4", Name="Disha"},
+            new StudentInfo(){ RollNo="5", Name="Rahul"},
+            new StudentInfo(){ RollNo="6", Name="Aniket"},
+            new StudentInfo(){ RollNo="7", Name="Vijaya"},
+            new StudentInfo(){ RollNo="8", Name="Rajeshwar"},
+            new StudentInfo(){ RollNo="9", Name="Ganesh"},
+            new StudentInfo(){ RollNo="10", Name="Mudra"},
+            new StudentInfo(){ RollNo="11", Name="Manjusha"},
+            new StudentInfo(){ RollNo="12", Name="Pankaj"},
+            new StudentInfo(){ RollNo="13", Name="Pooja"},
+            new StudentInfo(){ RollNo="14", Name="Suresh"},
+            new StudentInfo(){ RollNo="15", Name="Jaya"},
+            };
+
+
+            var sorted = list.Skip(skip).Take(take);
+
+
+            var n= new JsonResult() { 
+             Data=sorted,
+              JsonRequestBehavior=JsonRequestBehavior.AllowGet,
+              MaxJsonLength=list.Count
+            };
+
+
+             
+            return n;
+
         }
     }
+
+    public class StudentInfo
+    {
+        public string RollNo { get; set; }
+        public string Name { get; set; }
+    }
+
+
 }
--- DotNet/MCALWebAPIs/MCALWebAPIs/app/app.js
+++ DotNet/MCALWebAPIs/MCALWebAPIs/app/app.js
@@ -2,82 +2,49 @@
 
 angular.module('testApp').controller('testCtrl', function ($scope, $http, $timeout) {
 
-    $scope.activated = "UI Grid With Dynamic Columns";
+    $scope.activated = "UI Grid With Server Side filtering and paging !";
 
-    var portNumber = "32194";
+    var server = "http://localhost:";
+    var portNumber = "2185";
+    var partialURI = "/api/MCALAPIs/";
 
-    var getColumnsConfigURL = "http://localhost:" + portNumber + "/MDKServices.svc/getColumnsConfig";
-    var getAllStudentInfoURL = "http://localhost:" + portNumber + "/MDKServices.svc/getAllStudentInfo";
-
-    var procColumnConfigPersonalDetailsURL = "http://localhost:" + portNumber + "/MDKServices.svc/procColumnConfigPersonalDetails";
-    var getPersonalDetailsURL = "http://localhost:" + portNumber + "/MDKServices.svc/getPersonalDetails";
-
-    var procColumnConfigAcademicDetailsURL = "http://localhost:" + portNumber + "/MDKServices.svc/procColumnConfigAcademicDetails";
-    var getAcademicDetailsURL = "http://localhost:" + portNumber + "/MDKServices.svc/getAcademicDetails";
-
+    var getStudents = server + portNumber + partialURI + "getStudents";
 
     $scope.gridOptions = {
-        autoBind: false,
-        selectable: "row",
-        columns: $scope.gridColumns
+        columns: [
+            { "title": "Roll No", "field": "RollNo" },
+            { "title": "Name", "field": "Name" }
+
+        ],
+
+        pageable: true,
+        sortable: true
     };
 
 
-    $scope.allStudentInfo = function () {
-        $http.post(getColumnsConfigURL, {}).success(function (data, status) {
-            getGridGenericColumns(data.getColumnsConfigResult);
-        });
+    $scope.dsm = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: getStudents
+            }
 
-        $http.post(getAllStudentInfoURL, {}).success(function (data, status) {
-            $scope.gridData = data.getAllStudentInfoResult;
-             
-            $timeout(function () {
-                $scope.gridOptions.dataSource.data($scope.gridData);
-            }, 0);
+        },
+        pageSize: 5,
+        serverPaging: true,
+        serverFiltering: true,
 
+        schema: {
+            total: function (total) {
+                return total.MaxJsonLength;
+            },
+            data: "Data"
 
-        });
-    }
-
-
-    $scope.acedemicDetails = function () {
-        $http.post(procColumnConfigAcademicDetailsURL, {}).success(function (data, status) {
-             getGridGenericColumns(data.procColumnConfigAcademicDetailsResult);
-        });
-
-        $http.post(getAcademicDetailsURL, {}).success(function (data, status) {
-              
-            $scope.gridData = data.getAcademicDetailsResult;
-          
-            $timeout(function () {
-                $scope.gridOptions.dataSource.data($scope.gridData);
-            }, 0);
-
-
-        });
-    }
-
-
-    $scope.gridOptions.dataSource = new kendo.data.DataSource({
-         
+        }
     });
 
-     
-    var getGridGenericColumns = function (col) {
-        $scope.gridColumns = null;
-        $scope.gridColumns = [];
 
-        angular.forEach(col, function (value, key) {
-            $scope.gridColumns.push(new ColumnModel(value.ColumnName, value.ColumnName));
-        });
 
-        $scope.gridOptions.columns = $scope.gridColumns;
 
-    }
-     
-    function ColumnModel(field, title) {
-        this.field = field;
-        this.title = title;
-    }
-      
+
+
 });
