2e35a00542a46081aa68e0fe98c488469c9c08cd
AmitDebadwar <amit.debadwar@gmail.com>
2016-05-10 19:50:53 +0530

    DynamicColumnGrid with server side

--- DotNet/MCALWebAPIs/MCALWebAPIs/Controllers/MCALAPIsController.cs
+++ DotNet/MCALWebAPIs/MCALWebAPIs/Controllers/MCALAPIsController.cs
@@ -12,7 +12,37 @@ namespace MCALWebAPIs.Controllers
 {
     public class MCALAPIsController : ApiController
     {
-       
+
+
+        [Queryable]
+        public IQueryable GetAlbums()
+        {
+            var list = new List<StudentInfo>() { 
+          
+            new StudentInfo(){ RollNo="1", Name="Rajesh"},
+            new StudentInfo(){ RollNo="2", Name="Mahesh"},
+            new StudentInfo(){ RollNo="3", Name="Om"},
+            new StudentInfo(){ RollNo="4", Name="Disha"},
+            new StudentInfo(){ RollNo="5", Name="Rahul"},
+            
+            new StudentInfo(){ RollNo="6", Name="Aniket"},
+            new StudentInfo(){ RollNo="7", Name="Vijaya"},
+            new StudentInfo(){ RollNo="8", Name="Rajeshwar"},
+            new StudentInfo(){ RollNo="9", Name="Ganesh"},
+            new StudentInfo(){ RollNo="10", Name="Mudra"},
+            
+            new StudentInfo(){ RollNo="11", Name="Manjusha"},
+            new StudentInfo(){ RollNo="12", Name="Pankaj"},
+            new StudentInfo(){ RollNo="13", Name="Pooja"},
+            new StudentInfo(){ RollNo="14", Name="Suresh"},
+            new StudentInfo(){ RollNo="15", Name="Jaya"},
+            };
+
+            var albums = list.Select(x=>x.RollNo)
+
+        }
+
+
         public JsonResult getStudents()
         {
 
@@ -55,7 +85,7 @@ namespace MCALWebAPIs.Controllers
 
 
 
-            return n;
+            return Json<>(){   } json( );
 
         }
     }
