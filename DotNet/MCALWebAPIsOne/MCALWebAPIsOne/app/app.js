f112f10608ac604e45dd713c498180a0e6c463cc
AmitDebadwar <amit.debadwar@gmail.com>
2016-05-18 23:36:38 +0530

    first check in without kendo

---
+++ DotNet/MCALWebAPIsOne/MCALWebAPIsOne/app/app.js
@@ -0,0 +1,58 @@
+﻿var app = angular.module('testApp', ['ui.bootstrap', 'ngRoute', 'kendo.directives']);
+
+angular.module('testApp').controller('testCtrl', function ($scope, $http, $timeout) {
+
+    $scope.activated = "1) Paging !";
+
+    var server = "http://localhost:";
+    var portNumber = "2185";
+    var partialURI = "/api/MCALAPIs/";
+
+    var getStudents = server + portNumber + partialURI + "filterStudent";
+
+    /*paging without database*/
+    $scope.gridOptions = {
+        columns: [
+            { "title": "Roll No", "field": "RollNo" },
+            { "title": "Name", "field": "Name" }
+
+        ],
+
+        pageable: true,
+        sortable: true
+    };
+    $scope.dsm = new kendo.data.DataSource({
+        type:'odata',
+        transport: {
+            read: {
+                url: getStudents,
+                dataType: 'json'
+            }
+
+        },
+        parameterMap: function (data, type) {
+            var paramMap = kendo.data.transports.odata.parameterMap(options);
+            delete paramMap.$inlinecount; // <-- remove inlinecount parameter.
+            delete paramMap.$format; // <-- remove format parameter.
+            return paramMap;
+        },
+        pageSize: 5,
+        serverPaging: true,
+        serverFiltering: true,
+
+
+        schema: {
+            total: function (total) {
+                return total.MaxJsonLength;
+            },
+            data: "Data"
+
+        }
+    });
+    /*paging without database end*/
+
+
+
+
+
+});
---
+++ DotNet/MCALWebAPIsOne/MCALWebAPIsOne/app/backupAppJS.js
@@ -0,0 +1,126 @@
+﻿
+
+$scope.allStudentInfo = function () {
+    $http.get(getStudents, {}).success(function (data, status) {
+        console.log(data);
+    });
+
+
+}
+
+
+$scope.gridDataSource = {
+    transport: {
+        read: function (e) {
+            $http.get(getStudents, {}).success(function (data, status) {
+                console.log(e);
+                e.success(data.Data);
+            });
+
+        }
+    },
+
+    schema: {
+        total: function (response) {
+            console.log(response.Data);
+            return response.Data
+        },
+        model: {
+            id: "RollNo",
+            fields: {
+
+                'Name': { type: "string", editable: true, nullable: false, validation: { required: true } },
+                'RollNo': { type: "string", editable: true, nullable: false, validation: { required: true } }
+            }
+        }
+    },
+
+
+    pageSize: 2,
+    serverPaging: true,
+    serverFiltering: true
+
+
+
+};
+
+
+
+$scope.gridDataSource786 = {
+    transport: {
+        read: function (response) {
+            $http.get(getStudents, {}).success(function (data, status) {
+                console.log(data.Data);
+                response.success(data.Data);
+            });
+        }
+    },
+    pageSize: 2,
+    serverPaging: true,
+    serverFiltering: true
+
+
+
+};
+
+
+$scope.dsm1 = new kendo.data.DataSource({
+    transport: {
+        read: function (e) {
+
+            var requestData = {
+                page: e.data.page,
+                pageSize: e.data.pageSize,
+                type: "hello"
+            };
+
+
+            $http.get(getStudents, { data: requestData }).success(function (data, status) {
+                console.log(data.Data);
+                e.success(data);
+            });
+        }
+    },
+    pageSize: 2,
+    serverPaging: true,
+    serverFiltering: true,
+
+    schema: {
+        total: function (total) {
+            return total.Data.length;
+
+        },
+        data: "Data"
+
+    }
+});
+
+
+
+$scope.gridDataSource123 = {
+    transport: {
+        read: function (options) {
+            var webapi = new kendo.data.transports.webapi({ prefix: "" });
+            var params = webapi.parameterMap(options.data);
+
+            $http.get(getStudents, {})
+                .success(function (data) {
+                    options.success(data);
+                    
+                })
+                .error(function (data) {
+                    options.error();
+                });
+        }
+    },
+    pageSize: 10,
+    serverPaging: true,
+    serverSorting: true,
+    serverFiltering: true,
+    serverGrouping: true,
+    serverAggregates: true,
+    schema: $.extend({}, kendo.data.schemas.webapi, { data: "Data", total: "Total", errors: "Errors" })
+};
+
+
+
