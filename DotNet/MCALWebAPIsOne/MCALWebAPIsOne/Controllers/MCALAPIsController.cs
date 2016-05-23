485af6aa8e42eb7efbdf65464956b91bfa2d5f2e
AmitDebadwar <amit.debadwar@gmail.com>
2016-05-24 04:09:32 +0530

    File upload in angular grid is done. saving of the row object is remaining

--- DotNet/MCALWebAPIsOne/MCALWebAPIsOne/Controllers/MCALAPIsController.cs
+++ DotNet/MCALWebAPIsOne/MCALWebAPIsOne/Controllers/MCALAPIsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -44,6 +45,46 @@ namespace MCALWebAPIsOne.Controllers
 
         }
 
+        
+        public string uploadIcon()
+        {
+            int iUploadedCnt = 0;
+
+            string sPath = "";
+            sPath = System.Web.Hosting.HostingEnvironment.MapPath("~/uploadedIcons/");
+            System.Web.HttpFileCollection hfc = System.Web.HttpContext.Current.Request.Files;
+
+            // CHECK THE FILE COUNT.
+            for (int iCnt = 0; iCnt <= hfc.Count - 1; iCnt++)
+            {
+                System.Web.HttpPostedFile hpf = hfc[iCnt];
+
+                if (hpf.ContentLength > 0)
+                {
+                    // CHECK IF THE SELECTED FILE(S) ALREADY EXISTS IN FOLDER. (AVOID DUPLICATE)
+                    if (!File.Exists(sPath + Path.GetFileName(hpf.FileName)))
+                    {
+                        // SAVE THE FILES IN THE FOLDER.
+                        hpf.SaveAs(sPath + Path.GetFileName(hpf.FileName));
+                        iUploadedCnt = iUploadedCnt + 1;
+                    }
+                }
+            }
+
+            // RETURN A MESSAGE (OPTIONAL).
+            if (iUploadedCnt > 0)
+            {
+                //return iUploadedCnt + " Files Uploaded Successfully";
+                return "test\\path\\"+hfc[0].FileName;
+            }
+            else
+            {
+                return "Upload Failed";
+            }
+
+
+         
+        }
 
         public class StudentInfo
         {
--- DotNet/MCALWebAPIsOne/MCALWebAPIsOne/app/angGridFileUpload.js
+++ DotNet/MCALWebAPIsOne/MCALWebAPIsOne/app/angGridFileUpload.js
@@ -1,33 +1,50 @@
-﻿var app = angular.module('testApp', ['ui.bootstrap', 'ngRoute', 'kendo.directives']);
+﻿var app = angular.module('testApp', ['ui.bootstrap', 'ngRoute', 'kendo.directives', 'ngFileUpload']);
 
-angular.module('testApp').controller('testCtrl', function ($scope, $http, $timeout) {
+angular.module('testApp').controller('testCtrl', function ($scope, $http, $timeout,Upload) {
 
-    $scope.activated = "Sainath";
+    $scope.activated = "Grid View With File Upload";
 
     var server = "http://localhost:";
     var portNumber = "1308";
     var partialURI = "/api/MCALAPIs/";
 
-    var getStudents = server + portNumber + partialURI + "getStudents";
+    var uploadIcon = server + portNumber + partialURI + "uploadIcon";
 
-  
+
     $scope.model = {
         contacts: [{
             id: 1,
-            name: "Ben",
-            age: 28
+            firstName: "Ben",
+            lastName: "Jackson",
+            foreColor: 'green',
+            bgColor: 'pink',
+            icon: 'myR',
+            filePath:''
         }, {
             id: 2,
-            name: "Sally",
-            age: 24
+            firstName: "Sally",
+            lastName: "Cruise",
+            foreColor: "darkorange",
+            bgColor: 'lightblue',
+            icon: 'myQ',
+            filePath: ''
+
         }, {
             id: 3,
-            name: "John",
-            age: 32
+            firstName: "John",
+            lastName: "Samily",
+            foreColor: "red",
+            bgColor: 'darkRed',
+            icon: 'myP',
+            filePath: ''
         }, {
             id: 4,
-            name: "Jane",
-            age: 40
+            firstName: "Jane",
+            lastName: "Doe",
+            foreColor: "darkpink",
+            bgColor: 'darkOrange',
+            icon: 'myN',
+            filePath: ''
         }],
         selected: {}
     };
@@ -45,14 +62,38 @@ angular.module('testApp').controller('testCtrl', function ($scope, $http, $timeo
     $scope.saveContact = function (idx) {
         console.log("Saving contact");
         $scope.model.contacts[idx] = angular.copy($scope.model.selected);
+
+
+        console.log($scope.model.selected);
         $scope.reset();
     };
 
     $scope.reset = function () {
         $scope.model.selected = {};
     };
+     
+    $scope.uploadDocument = function (newIcon, idx) {
+      
+         Upload.upload({
+            url: uploadIcon,
+            data: { file: newIcon }
+          
+        }).then(function (resp) {
+            console.log('Success ');// + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
 
+            $scope.model.selected.filePath = resp.data;
+            $scope.model.contacts[idx] = angular.copy($scope.model.selected);
+            
 
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ');//+ evt.config.data.file.name);
+        });
+
+    };
+     
 
 
 });
