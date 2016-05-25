02ac8e0d70ad9281ee04e410c9d2824806c545e7
AmitDebadwar <amit.debadwar@gmail.com>
2016-05-26 03:03:28 +0530

    icon creater

---
+++ DotNet/MCALWebAPIsOne/MCALWebAPIsOne/app/iconCreater.js
@@ -0,0 +1,38 @@
+﻿var app = angular.module('testApp', ['ui.bootstrap', 'ngRoute', 'kendo.directives', 'ngFileUpload']);
+
+angular.module('testApp').controller('testCtrl', function ($scope, $http, $timeout,Upload) {
+
+    $scope.activated = "iCon Creater Form";
+
+    var server = "http://localhost:";
+    var portNumber = "1308";
+    var partialURI = "/api/MCALAPIs/";
+
+    var uploadIcon = server + portNumber + partialURI + "uploadIcon";
+    $scope.completeUrl = uploadIcon;
+
+
+    var init = function () {
+        $scope.brandingName = "MCAL";
+        $scope.pageTitle = "MCAL Title";
+        $scope.foreColor = "#ff0000";
+        $scope.bgColor = "#0400ff";
+        $scope.logoUrl = 'images/apple.png';
+    };
+
+    init();
+
+    
+
+    $scope.removeLogo = function () {
+         $scope.logoUrl = 'images/noImage.png';
+    }
+
+    $scope.onSuccess = function (e) {
+        
+        $scope.logoUrl = '../uploadedIcons/'+  e.files[0].name;
+        $scope.$apply();
+        console.log($scope.logoUrl);
+    }
+     
+});
---
+++ DotNet/MCALWebAPIsOne/MCALWebAPIsOne/app/starter.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module('testApp', ['ui.bootstrap', 'ngRoute', 'kendo.directives', 'ngFileUpload']);
+
+angular.module('testApp').controller('testCtrl', function ($scope, $http, $timeout,Upload) {
+
+    $scope.activated = "starter";
+
+    var server = "http://localhost:";
+    var portNumber = "1308";
+    var partialURI = "/api/MCALAPIs/";
+
+    var uploadIcon = server + portNumber + partialURI + "uploadIcon";
+
+      
+
+
+});
